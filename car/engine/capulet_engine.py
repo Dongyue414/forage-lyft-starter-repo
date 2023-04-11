a1bafa4d8ff257fcd85308c0adb59fcfc03087a5
Dongyue414 <zhangdongyue84@gmail.com>
2023-04-10 20:58:25 -0400

    04/10/2023_v2

--- car/engine/capulet_engine.py
+++ car/engine/capulet_engine.py
@@ -1,9 +1,8 @@
-from car import Car
+from engine import Engine
 
 
-class CapuletEngine(Car):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
+class CapuletEngine(Engine):
+    def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
